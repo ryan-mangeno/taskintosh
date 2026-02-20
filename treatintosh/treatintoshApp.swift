71bb1aaf9ca93ce682680b4b4b386a3cc36699a1
ryan-mangeno <ryanmangeno@gmail.com>
2026-02-20 09:06:50 -0500

    Initial Commit

---
+++ treatintosh/treatintoshApp.swift
@@ -0,0 +1,17 @@
+//
+//  treatintoshApp.swift
+//  treatintosh
+//
+//  Created by Ryan Mangeno on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct treatintoshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
